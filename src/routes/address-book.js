7c3f617bee883874c577d167d0b337345224bc9b
dcharlie528 <dcharlie528@gmail.com>
2020-06-24 17:07:59 +0800

    0624lesson2

--- src/routes/address-book.js
+++ src/routes/address-book.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const db = require(__dirname + '/../db_connect2');
 
-router.get('/list/:page?', async (req, res)=>{
+router.get('/list/:page?', async (req, res) => {
     let page = parseInt(req.params.page) || 1;
     const perPage = 5; // 每一頁要顯示幾筆
-    const sql = `SELECT * FROM address_book LIMIT ${(page-1)*perPage}, ${perPage}`;
+    const sql = `SELECT * FROM address_book LIMIT ${(page - 1) * perPage}, ${perPage}`;
     const [r] = await db.query(sql);
-
-    res.json(r);
+    // res.json(r);
+    res.render('address-book/list', { rows: r });
 });
 
 module.exports = router;
